0aa2da5e78141e824a03e57124ee7e5c86b2cf46
Piegames <14054505+piegamesde@users.noreply.github.com>
2018-03-10 21:19:48 +0100

    Made ActionModule have states! (STILL NOT TESTED)
    It's lot more powerful. And not tested, so a lot more buggy too...

--- src/main/java/de/piegames/picontrol/module/ActionModule.java
+++ src/main/java/de/piegames/picontrol/module/ActionModule.java
@@ -1,10 +1,12 @@
 package de.piegames.picontrol.module;
 
 import java.io.IOException;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
+import com.google.common.base.Optional;
+import com.google.common.graph.EndpointPair;
 import com.google.common.graph.MutableValueGraph;
 import com.google.common.graph.ValueGraphBuilder;
 import com.google.gson.JsonObject;
@@ -14,30 +16,72 @@ import de.piegames.picontrol.state.ContextState;
 
 public class ActionModule extends Module {
 
-	protected Map<String, Action> commands = new HashMap<>();
+	// protected Map<Edge<String, String>, Map<String, Action>> commands = new HashMap<>();
+	protected MutableValueGraph<String, Map<String, Action>> commands = ValueGraphBuilder.directed().build();
 
+	@SuppressWarnings("deprecation")
 	public ActionModule(PiControl control, String name, JsonObject config) throws RuntimeException {
 		super(control, name, config);
-		config.getAsJsonObject("commands").entrySet().stream()
-				.forEach(e -> commands.put(e.getKey(), Action.fromJson(e.getValue().getAsJsonObject())));
+		config.getAsJsonObject("commands").entrySet()
+				.stream()
+				.forEach(e -> {
+					JsonObject value = e.getValue().getAsJsonObject();
+					String end = "end";
+					if (value.has("next-state"))
+						end = value.getAsJsonPrimitive("next-state").getAsString();
+					putCommand("root", end, e.getKey(), Action.fromJson(value));
+				});
+		config.entrySet()
+				.stream()
+				.filter(e -> e.getKey().startsWith("commands-"))
+				.forEach(e -> {
+					JsonObject value = e.getValue().getAsJsonObject();
+					String end = "end";
+					if (value.has("next-state"))
+						end = value.getAsJsonPrimitive("next-state").getAsString();
+					putCommand(e.getKey().substring("commands-".length()), end, e.getKey(), Action.fromJson(value));
+				});
+	}
+
+	protected void putCommand(String start, String end, String command, Action value) {
+		if (!commands.hasEdgeConnecting("root", end))
+			commands.edgeValue("root", end).orElseGet(() -> {
+				HashMap<String, Action> ret = new HashMap<>();
+				commands.putEdgeValue(start, end, ret);
+				return ret;
+			}).put(command, value);
 	}
 
 	@Override
 	public MutableValueGraph<ContextState<Module>, Set<String>> listCommands(ContextState<Module> root) {
+		Map<String, ContextState<Module>> states = commands
+				.nodes()
+				.stream()
+				.filter(s -> !s.equals("root"))
+				.collect(Collectors.toMap(s -> s, s -> new ContextState<>(this, s)));
+		states.put("root", root);
+
 		MutableValueGraph<ContextState<Module>, Set<String>> ret = ValueGraphBuilder.directed().build();
-		ContextState<Module> node = new ContextState<>(this, "end");
-		ret.putEdgeValue(root, node, Collections.unmodifiableSet(commands.keySet()));
+		// ret.putEdgeValue(root, node, Collections.unmodifiableSet(startCommands.keySet()));
+		for (EndpointPair<String> edge : commands.edges()) {
+			ret.putEdgeValue(states.get(edge.source()), states.get(edge.target()), commands.edgeValue(edge.source(), edge.target()).get().keySet());
+		}
 		return ret;
 	}
 
 	@Override
 	public void onCommandSpoken(ContextState<Module> currentState, String command) {
 		try {
-			commands.get(command).execute(control);
+			for (String nextState : commands.successors(currentState.name)) {
+				Optional<Action> action = Optional.fromNullable(commands.edgeValue(currentState.name, nextState).get().get(command));
+				if (action.isPresent()) {
+					action.get().execute(control);
+					return;
+				}
+			}
+			log.warn("Command " + command + " is not registered for state " + currentState.name);
 		} catch (IOException e) {
 			log.warn("Could not execute command '" + command + "'", e);
-		} catch (NullPointerException e) {
-			log.warn("Command " + command + " is not registered");
 		} catch (InterruptedException e) {
 			log.warn("Could not wait for process to finish, it will continue running in background");
 		}
