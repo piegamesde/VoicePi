ef153778475e31f24cf215ffde0a032289aafed3
Unknown <saibotk@users.noreply.github.com>
2018-04-11 13:47:47 +0200

    Fix SpeechRecognizer

--- src/main/java/de/piegames/voicepi/stt/SpeechRecognizer.java
+++ src/main/java/de/piegames/voicepi/stt/SpeechRecognizer.java
@@ -7,7 +7,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.BlockingQueue;
-import javax.swing.event.ChangeListener;
+import javafx.beans.value.ChangeListener;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import com.google.gson.JsonElement;
